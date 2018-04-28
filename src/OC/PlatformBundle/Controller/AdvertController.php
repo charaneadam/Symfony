1dfb32cf484ac6e8d71b56a962b9c835edc4f386
Adam Charane <adamcharane@gmail.com>
2018-04-28 01:39:17 +0100

    Working on the skeleton of the twig templates (some raw content added)

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -12,11 +12,46 @@ class AdvertController extends Controller
         if($page < 1){
             throw new NotFoundHttpException("Page " . $page . " inexistante");
         }
-        return $this->render('OCPlatformBundle:Advert:index.html.twig');
+        
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+        
+        //return $this->render('OCPlatformBundle:Advert:index.html.twig');
+        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
     }
 
     public function viewAction($id){
-        return $this->render('OCPlatformBundle:Advert:view.html.twig', array('id' => $id));
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+        
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));
     }
     
     public function addAction(Request $request){
@@ -24,7 +59,18 @@ class AdvertController extends Controller
             $request->getSession()->getFlashBag('notice', 'Annonce bien enregistrée.');
             return $this->redirectToRoute('oc_platform_view', array('id' => 5));
         }
-        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+        
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => 5,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+        
+        return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
+            'advert' => $advert
+        ));
     }
     
     public function editAction($id, Request $request){
@@ -32,10 +78,35 @@ class AdvertController extends Controller
             $request->getSession()->getFlashBag('notice', 'Annonce bien modifiée.');
             return $this->redirectToRoute('oc_platform_view', array('id' => 5));
         }
-        return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
+            'advert' => $advert
+        ));
     }
     
     public function deleteAction($id){
         return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+    }
+    
+    public function menuAction($limit){
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+        
+        return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe
+            // les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
     }
 }
